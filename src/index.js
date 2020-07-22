33e762d9478a350a2e94d2e09765e8b19727fb86
Vadim Nazarov <vadim-nazarov-web@yandex.ru>
2020-07-22 16:01:04 +0300

    Маска для телефона

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import showNumbers from './modules/showNumbers';
 import burgerMenu from './modules/burgerMenu';
 import smoothScroll from './modules/smoothScroll';
-import popupRepairTypes from './modules/popupRepairTypes'
+import popupRepairTypes from './modules/popupRepairTypes';
+import phoneMask from './modules/phoneMask';
+
+
 
 showNumbers();
 burgerMenu();
 smoothScroll();
 popupRepairTypes();
+phoneMask();
