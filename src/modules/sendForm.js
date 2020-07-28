e17e535d629c8e5ceb5059a3449b0e6afeb66909
Vadim Nazarov <vadim-nazarov-web@yandex.ru>
2020-07-28 17:09:04 +0300

    352

--- src/modules/sendForm.js
+++ src/modules/sendForm.js
@@ -8,10 +8,11 @@ const sendForm = () => {
             const formData = new FormData(item);
             let body = {};
             formData.forEach((value,key) => body[key] = value);
-            postData(body)
+            if(item.querySelector('.checkbox__input').checked){
+                postData(body)
                 .then((response) => {
                     if(response.status !== 200) throw new Error('status network not 200')
-                    if(item.querySelector('.checkbox__input').checked){
+                    
                         setTimeout(() => {
                             document.querySelector('.popup-thank').style.cssText = 'visibility: visible; z-index:9999';
                             item.querySelector('.checkbox__input').checked = false;
@@ -23,27 +24,26 @@ const sendForm = () => {
                                 document.querySelector('.popup-thank').style.visibility = 'hidden'
                             }
                         })
-                    } else{
-                        const style = document.createElement('style');
-                        style.textContent = `
-                            .errorMessage{
-                                color: red !important;
-                                position: absolute;
-                                bottom: 0;
-                            }
-                        `;
-                        document.querySelector('header').append(style);
-                        const errMsg = document.createElement('div');
-                        errMsg.insertAdjacentHTML('beforeend', `<span>Прочтите Пользовательское соглашение</span>`);
-                        errMsg.classList.add('errorMessage');
-                        item.querySelector('.checkbox').parentNode.append(errMsg);
-                        setTimeout(() => errMsg.innerHTML = '', 3000);
-                        item.querySelectorAll('input').forEach(input => input.value = '')
-
-                    }
-                    
                 })
                 .catch(error => console.error(error));
+            } else{
+                const style = document.createElement('style');
+                style.textContent = `
+                    .errorMessage{
+                        color: red !important;
+                        position: absolute;
+                        bottom: 0;
+                    }
+                `;
+                document.querySelector('header').append(style);
+                const errMsg = document.createElement('div');
+                errMsg.insertAdjacentHTML('beforeend', `<span>Прочтите Пользовательское соглашение</span>`);
+                errMsg.classList.add('errorMessage');
+                item.querySelector('.checkbox').parentNode.append(errMsg);
+                setTimeout(() => errMsg.innerHTML = '', 3000);
+                item.querySelectorAll('input').forEach(input => input.value = '')
+            }
+            
         });
     });
     feedbackBlockForm.forEach(item => {
@@ -52,10 +52,10 @@ const sendForm = () => {
             const formData = new FormData(item);
             let body = {};
             formData.forEach((value,key) => body[key] = value);
-            postData(body)
+            if(item.querySelector('.checkbox__input').checked){
+                postData(body)
                 .then((response) => {
                     if(response.status !== 200) throw new Error('status network not 200')
-                    if(item.querySelector('.checkbox__input').checked){
                         setTimeout(() => {
                             document.querySelector('.popup-thank').style.cssText = 'visibility: visible; z-index:9999';
                             item.querySelector('.checkbox__input').checked = false;
@@ -67,27 +67,27 @@ const sendForm = () => {
                                 document.querySelector('.popup-thank').style.visibility = 'hidden'
                             }
                         })
-                    } else{
-                        const style = document.createElement('style');
-                        style.textContent = `
-                            .errorMessage{
-                                color: red !important;
-                                position: absolute;
-                                bottom: -10px;
-                            }
-                        `;
-                        document.querySelector('header').append(style);
-                        const errMsg = document.createElement('div');
-                        errMsg.insertAdjacentHTML('beforeend', `<span>Прочтите Пользовательское соглашение</span>`);
-                        errMsg.classList.add('errorMessage');
-                        item.querySelector('.checkbox').parentNode.append(errMsg);
-                        setTimeout(() => errMsg.innerHTML = '', 3000);
-                        item.querySelectorAll('input').forEach(input => input.value = '')
 
-                    }
-                    
                 })
                 .catch(error => console.error(error));
+            } else{
+                const style = document.createElement('style');
+                style.textContent = `
+                    .errorMessage{
+                        color: red !important;
+                        position: absolute;
+                        bottom: -10px;
+                    }
+                `;
+                document.querySelector('header').append(style);
+                const errMsg = document.createElement('div');
+                errMsg.insertAdjacentHTML('beforeend', `<span>Прочтите Пользовательское соглашение</span>`);
+                errMsg.classList.add('errorMessage');
+                item.querySelector('.checkbox').parentNode.append(errMsg);
+                setTimeout(() => errMsg.innerHTML = '', 3000);
+                item.querySelectorAll('input').forEach(input => input.value = '')
+            }
+            
         });
     })
 
